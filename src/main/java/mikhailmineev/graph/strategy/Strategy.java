af35d48a5879e200505b29f7cebdec551197d6fc
Mikhail Mineev <mikhailmineev@icloud.com>
2023-02-02 23:14:05 +0400

    Add solver without statistics

--- src/main/java/mikhailmineev/graph/strategy/Strategy.java
+++ src/main/java/mikhailmineev/graph/strategy/Strategy.java
@@ -2,11 +2,26 @@ package mikhailmineev.graph.strategy;
 
 import mikhailmineev.graph.core.Node;
 import mikhailmineev.graph.solution.Route;
+import mikhailmineev.graph.stats.NoStatistics;
 import mikhailmineev.graph.stats.StatisticsWriter;
 
 import java.util.Map;
 
 public interface Strategy {
+
+    /**
+     * Calculate a route from location A to location B in a graph.
+     * 
+     * @see Strategy#findRoute(String, String, Map, StatisticsWriter) 
+     *
+     * @param from the beginning location
+     * @param to destination
+     * @param graph graph to perform search on
+     * @return route from location A to location B
+     */
+    default Route findRoute(String from, String to, Map<String, Node> graph) {
+        return findRoute(from, to, graph, new NoStatistics());
+    }
 
     /**
      * Calculate a route from location A to location B in a graph.
